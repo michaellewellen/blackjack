7cb09f816c49af2991b22538c93f79df97d47cca
michael.lewellen <michael.lewellen@snow.edu>
2023-03-02 16:46:21 -0700

    Turned Cards into a class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿Stack<Card> deckOfCards = deckOfCards = shuffleCards();
+﻿Stack<Card> deckOfCards = shuffleCards();
 List<Card> dealersHand = new List<Card>();
 List<Card> playersHand = new List<Card>();
+Console.Clear();
+loadMenu();
+string playAgain = "Y";
+int winnings = 500;
+int bet = 0;
+while (playAgain.ToUpper() != "Q")
+{
+    bet = betScreen(winnings);
 
-dealersHand = getCards(ref deckOfCards, dealersHand,2,true);
-playersHand = getCards(ref deckOfCards, playersHand,2,false);
-
+}
 
 Stack<Card> shuffleCards()
 {
@@ -23,27 +29,100 @@ Stack<Card> shuffleCards()
         }
         nums[i] = num;
         Card temp = new Card(num);
-        deck.Add(temp);
+        deck.Push(temp);
     }
     return deck;
 }
 List<Card> getCards(ref Stack<Card> deck, List<Card> cards,int num, bool isDealer)
 {
     Card temp;
-    for (int i = deck.Count(); i< num; i++)
+    int size = cards.Count();
+    for (int i = cards.Count(); i< num+size; i++)
     {
         temp = deck.Pop();
         if (i==0 && isDealer == true)
             temp.hidden = true;
         if (isDealer)
-            temp.xValue = 0;
+            temp.yValue = 0;
         else 
-            temp.xValue = 10;
-        temp.yValue = i * 5;
+            temp.yValue = 8;
+        temp.xValue = i * 5;
         cards.Add(temp);
       
     }
     return cards;
+}
+void loadMenu()
+{
+    Console.Clear();
+    string welcome = @"
+  _______ __            __    _______            __    __ __ 
+ |   _   |  .---.-.----|  |--|   _   .---.-.----|  |--|  |  |
+ |.  1   |  |  _  |  __|    <|___|   |  _  |  __|    <|__|__|
+ |.  _   |__|___._|____|__|__|.  |   |___._|____|__|__|__|__|
+ |:  1    \                  |:  1   |                       
+ |::.. .  /                  |::.. . |                       
+ `-------'                   `-------'                       ";
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(welcome);
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine("\n              Press any key to continue");
+    Console.ReadKey(true);
+
+}
+int betScreen(int x)
+{
+    string placeYourBet = @"
+    The game is Black Jack, your goal is to get 21.  If you do it in two cards thats a blackjack
+    pays 3/2 regular win doubles your bet. The house has to hit on 16, stay on 17
+    
+    ";
+    Console.Clear();
+    Stack<Card> deck = shuffleCards();
+    List<Card> topRow = new List<Card>();
+    List<Card> bottomRow = new List<Card>();
+    topRow = getCards(ref deck,topRow,20,true);
+    bottomRow = getCards(ref deck,bottomRow,20,false);
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.Red;
+    for (int i = 0; i<20; i++)
+    {
+        topRow[i].hidden = true;
+        bottomRow[i].hidden = true;
+        
+        topRow[i].printCard();
+        bottomRow[i].printCard();
+    }
+
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.SetCursorPosition(0,15);
+    Console.WriteLine(placeYourBet);
+    Console.Write($"How much of your ${x} do you want to bet? ");
+    bool validBet = false;
+    int wager = 0;
+    while(!validBet)
+    {
+        validBet = Int32.TryParse(Console.ReadLine(), out wager);
+        if (!validBet)
+        {
+            Console.WriteLine("Partner you'll find a number works best");
+        }
+        else if (wager == 0)
+        {
+            Console.WriteLine("Ain't much point in playing if you don't bet anything.");
+            validBet = false;
+        }
+        else if (wager < 0 || wager > x)
+        {
+            Console.WriteLine("You just don't have that kinda money");
+            validBet = false;
+        }
+    }
+    return wager;
 }
 
 class Card
@@ -146,24 +225,39 @@ class Card
                 return 0;
         }
     }
-    void printCard()
+    public void printCard()
     {
+        Console.SetCursorPosition(xValue,yValue);
+        Console.SetCursorPosition(xValue,yValue);
+        Console.SetCursorPosition(xValue,yValue);
         Console.SetCursorPosition(xValue,yValue);
         if(hidden)
         {
-            Console.WriteLine("┌─────┐");
-            Console.WriteLine("│▒▒▒▒▒│");
-            Console.WriteLine("│▒▒▒▒▒│");
-            Console.WriteLine("│▒▒▒▒▒│");
-            Console.WriteLine("└─────┘");
+            Console.WriteLine("┌───────┐");   
+            Console.SetCursorPosition(xValue,yValue+1);
+            Console.WriteLine("│▒▒▒▒▒▒▒│");
+            Console.SetCursorPosition(xValue,yValue+2);
+            Console.WriteLine("│▒▒▒▒▒▒▒│");
+            Console.SetCursorPosition(xValue,yValue+3);
+            Console.WriteLine("│▒▒▒▒▒▒▒│");
+            Console.SetCursorPosition(xValue,yValue+4);
+            Console.WriteLine("│▒▒▒▒▒▒▒│");
+            Console.SetCursorPosition(xValue,yValue+5);
+            Console.WriteLine("└───────┘");
         }
         else
         {
-            Console.WriteLine("┌─────┐");
-            Console.WriteLine($"│{number}{suit}   │");
-            Console.WriteLine("│     │");
-            Console.WriteLine("│     │");
-            Console.WriteLine("└─────┘");
+            Console.WriteLine("┌───────┐");   
+            Console.SetCursorPosition(xValue,yValue+1);
+            Console.WriteLine($"│{number}{suit}     │");
+            Console.SetCursorPosition(xValue,yValue+2);
+            Console.WriteLine("│       │");
+            Console.SetCursorPosition(xValue,yValue+3);
+            Console.WriteLine($"│   {suit}   │");
+            Console.SetCursorPosition(xValue,yValue+4);
+            Console.WriteLine("│       │");
+            Console.SetCursorPosition(xValue,yValue+5);
+            Console.WriteLine("└───────┘");            
         }
     }
     public char suit;
